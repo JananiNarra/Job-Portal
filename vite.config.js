b85099b377bea07bfa72a0b8b6b250a831b0bca3
JananiNarra <janani7981@gmail.com>
2025-08-03 21:41:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './', // ← ✅ This fixes broken imports on Netlify
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',  // file in root
+          dest: ''            // copied to dist/
+        }
+      ]
+    })
+  ]
 })
